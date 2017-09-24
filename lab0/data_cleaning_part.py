cb88066664034a83144470cd28281728a5e8757b
Sadip Giri <sadipgiri@Sadips-MacBook-Pro.local>
2017-09-24 11:09:33 -0400

    cleaned CatalogDump.txt file
    read that cleaned file as css using pandas module of python

---
+++ lab0/data_cleaning_part.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+	my_data_splitter.py - Python3 program
+	Author: Sadip Giri (sadipgiri@bennington.edu)
+	Created: 09/11/2017
+"""
+
+import pandas as pd
+
+def convert_to_csv(fileName):
+	# create an empty file
+	new_clean_file = open("clean_CatalogDump.txt", "w")
+
+	with open(fileName) as my_file:
+		for i in my_file:
+			line = i.split("\t")	
+			values = line[0:8]	# just extracting the columns/fields ignoring tabs
+			my_line = values[0] + "," + values[1] + "," + values[2] + "," + values[3] + "," + values[4] + "," + values[5] + "," + values[6] + "," + values[7] 
+			new_clean_file.write(my_line + '\n')	# finally dumped into clean file
+	
+	return new_clean_file	
+
+'''def showFieldsType(fileName):
+	my_file = pd.read_csv(convert_to_csv("CatalogDump.txt"))
+	print(my_file.dtypes)
+'''
+# did it!
+convert_to_csv("CatalogDump.txt")
+
+# Then in terminal: I did these steps:
+# import pandas as pd 	# importing this pandas module
+# f = pd.read_csv("clean_CatalogDump.txt")	# reading the file as CSV file
+# f.dtypes 	# it shows all the types of the fields
+
+# Error: when I wanted to change year-to-date circ field to integer
+# ValueError: invalid literal for int() with base 10: 'b1000158x'
+
+
+
+
